de897e9a28b331c5d5d9e1c38e2bf95d8da920be
jefrine correya <jefrinecorreya1234@gmail.com>
2025-04-26 23:13:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -54,6 +54,6 @@ def results():
 
     return render_template('index.html', result=result)
     if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+        import os
+        port = int(os.environ.get("PORT", 5000))
+        app.run(host="0.0.0.0", port=port)
